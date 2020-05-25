b67f425cea2a828fb3282d0b41d8e46046b9f593
Donna Mayfield <donna-mayfield@lambdastudents.com>
2020-05-25 15:34:36 -0700

    Initial commit of playground

--- Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
@@ -8,10 +8,64 @@ import Foundation
 //: The below example shows a protocol that requires conforming types have a particular property defined.
 
 
+// Getter - ability to read access
+// Setter - write access
+
+protocol fullyNamed {
+    var fullName: String { get }
+}
+
+// Step1 - Adoption : fullyNamed
+// Step2 - Conformance - adds the properties
+
+struct Person: fullyNamed {
+    var fullName: String
+}
+
+let Johny = Person(fullName: "Johnny Hicks")
+let Donna = Person(fullName: "Donna Mayfield")
+
+print(Donna.fullName)
+
+class Starship: fullyNamed {
+    var prefix: String?
+    var name: String
+    
+    init (name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    
+    // Computed property (calculated property)
+    var fullName: String {
+        // ternary operators
+        return (prefix != nil ? prefix! + " ": "") + self.name
+    }
+}
+
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+ncc1701.fullName
+ncc1701.prefix
+ncc1701.name
+
+var fireFly = Starship(name: "Serenity")
+fireFly.fullName
+fireFly.prefix
 
 //: Protocols can also require that conforming types implement certain methods.
 
+protocol GeneratesRandomNumbers {
+    func random() -> Int
+}
 
+class OneThroughTen: GeneratesRandomNumbers {
+    func random() -> Int {
+        return Int.random(in: 1...10)
+    }
+}
+
+let rand = OneThroughTen()
+rand.random()
 
 //: Using built-in Protocols
 
